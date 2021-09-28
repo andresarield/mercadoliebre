8dc2adb99913f7b0c9e1113fefd4df081c6a3f30
andresarield <dabo-st@hhotmail.com>
2021-09-28 18:43:52 -0300

    media y boxes y agregados

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+
+app.get('/register',(req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.post('/register', (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/');
+});
